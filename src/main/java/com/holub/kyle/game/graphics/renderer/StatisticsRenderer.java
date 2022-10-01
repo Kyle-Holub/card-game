f9737e1f9dd75014cefd1f8f80b3241b04217920
Kyle <kholub2@yahoo.com>
2022-09-30 22:11:28 -0500

    StatisticsRenderer update

--- src/main/java/com/holub/kyle/game/graphics/renderer/StatisticsRenderer.java
+++ src/main/java/com/holub/kyle/game/graphics/renderer/StatisticsRenderer.java
@@ -1,5 +1,22 @@
 package com.holub.kyle.game.graphics.renderer;
 
+import com.holub.kyle.game.graphics.handlers.Window;
+
 public class StatisticsRenderer {
 
+    private int graphX1;
+    private int graphY1;
+    private int graphX2;
+    private int graphY2;
+
+    private void init(Window w) {
+        int screenWidth = w.getWidth();
+        int screenHeight = w.getHeight();
+
+        
+    }
+    // update frequency in seconds
+    // subdivide screen into sections per update
+    // plot point for each section
+    // draw line between points
 }
